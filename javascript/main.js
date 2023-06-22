f72accf964d741251b8337ec0a2d0466ee583e50
Alexandre Foulc <alexandre-foulc@orange.fr>
2023-06-22 19:01:50 +0200

    Refactorisation du code JS

--- javascript/main.js
+++ javascript/main.js
@@ -1,174 +1,184 @@
-//  Définition de la fonction "aléatoire"
+/////////////////////////////////////////////////////////////////
+//   Déclaration des fonctions
+/////////////////////////////////////////////////////////////////
+
+// Fonction "aléatoire" du lancer de Dé
 function getRandomIntInclusive(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min + 1)) + min;
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-
-// Définition de la fonction pour déterminer le joueur qui démarre la partie de manière aléatoire
-function chooseBeginner() {
-  let i = getRandomIntInclusive(1, 2);
-  return i;
+// Fonction pour déterminer aléatoirement le joueur qui démarre
+function getBeginner() {
+	let i = getRandomIntInclusive(1, 2);
+	return i;
 }
-
-// Définition de la fonction pour afficher un point rouge à côté du joueur qui démarre
+// Fonction pour afficher un point rouge à côté du joueur "P"
 function displayPlayer(p) {
-  const player1 = document.querySelector(".redTag1");
-  const player2 = document.querySelector(".redTag2");
-
-  if (p === 1) {
-    player1.style.display = "inline-block";
-    player2.style.display = "none";
-  } else {
-    player1.style.display = "none";
-    player2.style.display = "inline-block";
-  }
+	const player1 = document.querySelector(".redTag1");
+	const player2 = document.querySelector(".redTag2");
+	if (p === 1) {
+		player1.style.display = "inline-block";
+		player2.style.display = "none";
+	} else {
+		player1.style.display = "none";
+		player2.style.display = "inline-block";
+	}
 }
-
-// Déclaration de la fonction "Roll Dice" pour lancer le dé.
+// Fonction "Roll Dice" pour lancer le dé.
 function rollDice() {
-  let i = getRandomIntInclusive(1, 6);
-  return i;
+	let i = getRandomIntInclusive(1, 6);
+	return i;
+}
+// Fonction "startNewGame" pour démarrer une nouvelle partie (remise des scores à zéro)
+function startNewGame() {
+	// Tirage au sort du joueur qui démarre la nouvelle partie
+	let firstPlayer = getBeginner();
+	displayPlayer(firstPlayer);
+	console.log(`Le joueur ${firstPlayer} commence la nouvelle partie`);
+	player = firstPlayer;
+	// Scores à zéro
+	globalPlayer1.innerText = 0;
+	globalPlayer2.innerText = 0;
+	roundPlayer1.innerText = 0;
+	roundPlayer2.innerText = 0;
+}
+// Fonction pour afficher les faces du Dé suivant le résultat
+function displayDice(result) {
+	//
+	// Les points rouges sont d'abord masqués puis affichés selon le résultat
+	dots11.classList.remove("dots__red");
+	dots13.classList.remove("dots__red");
+	dots21.classList.remove("dots__red");
+	dots22.classList.remove("dots__red");
+	dots23.classList.remove("dots__red");
+	dots31.classList.remove("dots__red");
+	dots33.classList.remove("dots__red");
+	if (result === 1) {
+		dots22.classList.add("dots__red");
+	} else if (result === 2) {
+		dots11.classList.add("dots__red");
+		dots33.classList.add("dots__red");
+	} else if (result === 3) {
+		dots11.classList.add("dots__red");
+		dots22.classList.add("dots__red");
+		dots33.classList.add("dots__red");
+	} else if (result === 4) {
+		dots11.classList.add("dots__red");
+		dots13.classList.add("dots__red");
+		dots31.classList.add("dots__red");
+		dots33.classList.add("dots__red");
+	}
+	if (result === 5) {
+		dots11.classList.add("dots__red");
+		dots13.classList.add("dots__red");
+		dots22.classList.add("dots__red");
+		dots31.classList.add("dots__red");
+		dots33.classList.add("dots__red");
+	} else if (result === 6) {
+		dots11.classList.add("dots__red");
+		dots13.classList.add("dots__red");
+		dots21.classList.add("dots__red");
+		dots23.classList.add("dots__red");
+		dots31.classList.add("dots__red");
+		dots33.classList.add("dots__red");
+	}
+}
+// Fonction "getRollDice" pour lancer le dé, afficher les points rouges du dé et tester si le résultat vaut 1
+function getRollDice() {
+	// Lancer du Dé
+	let result = rollDice();
+	// Affichage de la face du Dé
+	displayDice(result);
+	// Affichage du score dans le "Current" du joueur concerné.
+	// En plus : si résultat Dé =1, alors score du ROUND à zéro et changement de joueur
+	if (player === 1) {
+		if (result === 1) {
+			alert("dommage !");
+			round1 = 0;
+			roundPlayer1.innerText = round1;
+			displayPlayer(2);
+			player = 2;
+		} else {
+			round1 = round1 + result;
+			roundPlayer1.innerText = round1;
+		}
+	} else {
+		if (result === 1) {
+			alert("dommage !");
+			round2 = 0;
+			roundPlayer2.innerText = round2;
+			displayPlayer(1);
+			player = 1;
+		} else {
+			round2 = round2 + result;
+			roundPlayer2.innerText = round2;
+		}
+	}
+}
+// Fonction "savePointsToGlobal" pour additionner le score du ROUND au score GLOBAL
+// puis on teste le score GLOBAL : si >=100 alors changement de joueur
+function savePointsToGlobal() {
+	if (player === 1) {
+		global1 = global1 + round1;
+		if (global1 >= 100) {
+			alert(`Le GAGNANT est : PLAYER 1`);
+		} else {
+			globalPlayer1.innerText = global1;
+			displayPlayer(2);
+			player = 2;
+			round1 = 0;
+			roundPlayer1.innerText = round1;
+		}
+	} else {
+		global2 = global2 + round2;
+		if (global2 >= 100) {
+			alert(`Le GAGNANT est : PLAYER 2`);
+		} else {
+			globalPlayer2.innerText = global2;
+			displayPlayer(1);
+			player = 1;
+			round2 = 0;
+			roundPlayer2.innerText = round2;
+		}
+	}
 }
 
 /////////////////////////////////////////////////////////////////
 
-// Message de Bienvenue quand le DOM est chargé
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("Hello, voici mon jeu en JS !");
-});
+//   Déroulement du Script
 
-// Fonctionnalité du bouton "New Game" : remise des scores à zéro.
-const newGame = document.getElementById("btnNewGame");
+/////////////////////////////////////////////////////////////////
+
+// Message de Bienvenue quand le DOM est chargé
+// document.addEventListener("DOMContentLoaded", () => {
+// 	console.log("Hello, voici mon jeu en JS !");
+// });
+
+// Déclaration des variables du SCORE TOTAL des 2 joueurs
 const globalPlayer1 = document.getElementById("globalPlayer1");
 const globalPlayer2 = document.getElementById("globalPlayer2");
+
+// Déclaration des variables du SCORE du ROUND des 2 joueurs
 const roundPlayer1 = document.getElementById("roundPlayer1");
 const roundPlayer2 = document.getElementById("roundPlayer2");
-newGame.addEventListener("click", () => {
-  globalPlayer1.innerText = 0;
-  globalPlayer2.innerText = 0;
-  roundPlayer1.innerText = 0;
-  roundPlayer2.innerText = 0;
-  // Tirage au sort du joueur qui démarre la nouvelle partie
-  const firstPlayer = chooseBeginner();
-  displayPlayer(firstPlayer);
-  console.log(`Le joueur ${firstPlayer} commence la nouvelle partie`);
-  player = firstPlayer;
-});
 
-// Fonctionnalité du bouton "RollDice"
-const btnRollDice = document.querySelector("#btnRollDice");
-btnRollDice.addEventListener("click", () => {
-  let result = rollDice();
-  // Ci-dessous, vérification du bon déroulement du Script dans la console :
-  console.log(`Lancé de Dé ${result}, joueur en cours: PLAYER ${player}`);
+// Déclarations des variables pour l'affichage de la face du Dé
+const dots11 = document.querySelector(".dots11");
+const dots13 = document.querySelector(".dots13");
+const dots21 = document.querySelector(".dots21");
+const dots22 = document.querySelector(".dots22");
+const dots23 = document.querySelector(".dots23");
+const dots31 = document.querySelector(".dots31");
+const dots33 = document.querySelector(".dots33");
 
-  // Ci-après, Affichage des faces du Dé suivant le résultat du lancé :
-  const dots11 = document.querySelector(".dots11");
-  const dots13 = document.querySelector(".dots13");
-  const dots21 = document.querySelector(".dots21");
-  const dots22 = document.querySelector(".dots22");
-  const dots23 = document.querySelector(".dots23");
-  const dots31 = document.querySelector(".dots31");
-  const dots33 = document.querySelector(".dots33");
-
-  dots11.classList.remove("dots");
-  dots13.classList.remove("dots");
-  dots21.classList.remove("dots");
-  dots22.classList.remove("dots");
-  dots23.classList.remove("dots");
-  dots31.classList.remove("dots");
-  dots33.classList.remove("dots");
-
-  if (result === 1) {
-    dots22.classList.add("dots");
-  } else if (result === 2) {
-    dots11.classList.add("dots");
-    dots33.classList.add("dots");
-  } else if (result === 3) {
-    dots11.classList.add("dots");
-    dots22.classList.add("dots");
-    dots33.classList.add("dots");
-  } else if (result === 4) {
-    dots11.classList.add("dots");
-    dots13.classList.add("dots");
-    dots31.classList.add("dots");
-    dots33.classList.add("dots");
-  }
-  if (result === 5) {
-    dots11.classList.add("dots");
-    dots13.classList.add("dots");
-    dots22.classList.add("dots");
-    dots31.classList.add("dots");
-    dots33.classList.add("dots");
-  } else if (result === 6) {
-    dots11.classList.add("dots");
-    dots13.classList.add("dots");
-    dots21.classList.add("dots");
-    dots23.classList.add("dots");
-    dots31.classList.add("dots");
-    dots33.classList.add("dots");
-  }
-
-  // Affichage du résultat du lancer de Dé dans le "Current" du joueur concerné.
-  // En plus : si résultat Dé =1, alors changement de joueur
-  if (player === 1) {
-    if (result === 1) {
-      round1 = 0;
-      roundPlayer1.innerText = round1;
-      displayPlayer(2);
-      player = 2;
-    } else {
-      round1 = result;
-      roundPlayer1.innerText = round1;
-    }
-  } else {
-    if (result === 1) {
-      round2 = 0;
-      roundPlayer2.innerText = round2;
-      displayPlayer(1);
-      player = 1;
-    } else {
-      round2 = result;
-      roundPlayer2.innerText = round2;
-    }
-  }
-});
-
-// Fonctionnalité du bouton HOLD : additionner le 'Current' au 'Global', test si >=100, et changement de joueur
-const btnHold = document.querySelector("#btnHold");
-btnHold.addEventListener("click", () => {
-  console.log(`Click sur le bouton HOLD, joueur en cours: ${player}`);
-  if (player === 1) {
-    global1 = global1 + round1;
-    if (global1 >= 100) {
-      alert(`Le GAGNANT est : PLAYER 1`);
-    } else {
-      globalPlayer1.innerText = global1;
-      displayPlayer(2);
-      player = 2;
-      round1 = 0;
-      roundPlayer1.innerText = round1;
-    }
-  } else {
-    global2 = global2 + round2;
-    if (global2 >= 100) {
-      alert(`Le GAGNANT est : PLAYER 2`);
-    } else {
-      globalPlayer2.innerText = global2;
-      displayPlayer(1);
-      player = 1;
-      round2 = 0;
-      roundPlayer2.innerText = round2;
-    }
-  }
-});
-
-const firstPlayer = chooseBeginner();
-displayPlayer(firstPlayer);
-console.log(`Le joueur PLAYER ${firstPlayer} commence la partie`);
-let player = firstPlayer;
+// Déclaration des variables modifiables des scores ROUND et GLOBAL
 let global1 = 0;
 let global2 = 0;
 let round1 = 0;
 let round2 = 0;
+
+// Affichage du joueur qui démarre la partie (aléatoirement)
+let firstPlayer = getBeginner();
+displayPlayer(firstPlayer);
+let player = firstPlayer;
