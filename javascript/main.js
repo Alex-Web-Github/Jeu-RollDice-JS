2823bea33c4299ab321ee1e117eaf44630649433
Alexandre Foulc <alexandre-foulc@orange.fr>
2023-06-23 11:18:05 +0200

    TimeOut sur le JS and upgrading Media Query on Mobile

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,50 @@
-/////////////////////////////////////////////////////////////////
-//   Déclaration des fonctions
-/////////////////////////////////////////////////////////////////
+// Déclaration des variables du SCORE TOTAL des 2 joueurs
+const globalPlayer1 = document.getElementById("globalPlayer1");
+const globalPlayer2 = document.getElementById("globalPlayer2");
 
+// Déclaration des variables du SCORE du ROUND des 2 joueurs
+const roundPlayer1 = document.getElementById("roundPlayer1");
+const roundPlayer2 = document.getElementById("roundPlayer2");
+
+// Déclarations des variables pour l'affichage de la face du Dé
+const dots11 = document.querySelector(".dots11");
+const dots13 = document.querySelector(".dots13");
+const dots21 = document.querySelector(".dots21");
+const dots22 = document.querySelector(".dots22");
+const dots23 = document.querySelector(".dots23");
+const dots31 = document.querySelector(".dots31");
+const dots33 = document.querySelector(".dots33");
+
+// Déclaration des variables modifiables des scores ROUND et GLOBAL
+let global1;
+let global2;
+let round1;
+let round2;
+
+// Fonction de Remise à zéro des scores et de l'affichage
+function resetGame() {
+	global1 = 0;
+	global2 = 0;
+	round1 = 0;
+	round2 = 0;
+	globalPlayer1.innerText = 0;
+	globalPlayer2.innerText = 0;
+	roundPlayer1.innerText = 0;
+	roundPlayer2.innerText = 0;
+}
 // Fonction "aléatoire" du lancer de Dé
 function getRandomIntInclusive(min, max) {
 	min = Math.ceil(min);
 	max = Math.floor(max);
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 // Fonction pour déterminer aléatoirement le joueur qui démarre
 function getBeginner() {
 	let i = getRandomIntInclusive(1, 2);
 	return i;
 }
+
 // Fonction pour afficher un point rouge à côté du joueur "P"
 function displayPlayer(p) {
 	const player1 = document.querySelector(".redTag1");
@@ -25,27 +57,25 @@ function displayPlayer(p) {
 		player2.style.display = "inline-block";
 	}
 }
-// Fonction "Roll Dice" pour lancer le dé.
+
+// Fonction pour lancer le dé.
 function rollDice() {
 	let i = getRandomIntInclusive(1, 6);
 	return i;
 }
-// Fonction "startNewGame" pour démarrer une nouvelle partie (remise des scores à zéro)
+
+// Fonction "startNewGame" (Button 'NEW GAME') pour démarrer une nouvelle partie (remise des scores à zéro)
 function startNewGame() {
+	// Reset des scores
+	resetGame();
 	// Tirage au sort du joueur qui démarre la nouvelle partie
 	let firstPlayer = getBeginner();
 	displayPlayer(firstPlayer);
-	console.log(`Le joueur ${firstPlayer} commence la nouvelle partie`);
 	player = firstPlayer;
-	// Scores à zéro
-	globalPlayer1.innerText = 0;
-	globalPlayer2.innerText = 0;
-	roundPlayer1.innerText = 0;
-	roundPlayer2.innerText = 0;
 }
+
 // Fonction pour afficher les faces du Dé suivant le résultat
 function displayDice(result) {
-	//
 	// Les points rouges sont d'abord masqués puis affichés selon le résultat
 	dots11.classList.remove("dots__red");
 	dots13.classList.remove("dots__red");
@@ -84,17 +114,18 @@ function displayDice(result) {
 		dots33.classList.add("dots__red");
 	}
 }
-// Fonction "getRollDice" pour lancer le dé, afficher les points rouges du dé et tester si le résultat vaut 1
-function getRollDice() {
-	// Lancer du Dé
-	let result = rollDice();
-	// Affichage de la face du Dé
-	displayDice(result);
-	// Affichage du score dans le "Current" du joueur concerné.
-	// En plus : si résultat Dé =1, alors score du ROUND à zéro et changement de joueur
+
+// Affichage du score dans le "Current" du joueur concerné.
+// En plus : si résultat Dé =1, alors score du ROUND à zéro et changement de joueur
+function testResult(result) {
 	if (player === 1) {
 		if (result === 1) {
-			alert("dommage !");
+			// Temporisation avant d'afficher la modale "alert()"
+			setTimeout(
+				alert,
+				500,
+				"VOUS OBTENEZ 1 --> Vous avez perdu tous vos points. Dommage ! "
+			);
 			round1 = 0;
 			roundPlayer1.innerText = round1;
 			displayPlayer(2);
@@ -105,7 +136,12 @@ function getRollDice() {
 		}
 	} else {
 		if (result === 1) {
-			alert("dommage !");
+			// Temporisation avant d'afficher la modale "alert()"
+			setTimeout(
+				alert,
+				500,
+				"VOUS OBTENEZ 1 --> Vous avez perdu tous vos points. Dommage ! "
+			);
 			round2 = 0;
 			roundPlayer2.innerText = round2;
 			displayPlayer(1);
@@ -116,13 +152,24 @@ function getRollDice() {
 		}
 	}
 }
-// Fonction "savePointsToGlobal" pour additionner le score du ROUND au score GLOBAL
+
+// Fonction "getRollDice" (Button 'ROLLDICE') pour lancer le dé, afficher le résultat et tester si le résultat vaut 1
+function getRollDice() {
+	// Lancer du Dé
+	let result = rollDice();
+	// Affichage de la face du Dé
+	displayDice(result);
+	// Affichage du score dans le "Current" du joueur concerné et testing 'result === 1'
+	testResult(result);
+}
+
+// Fonction "savePointsToGlobal" (Button 'HOLD') pour additionner le score du ROUND au score GLOBAL
 // puis on teste le score GLOBAL : si >=100 alors changement de joueur
 function savePointsToGlobal() {
 	if (player === 1) {
 		global1 = global1 + round1;
 		if (global1 >= 100) {
-			alert(`Le GAGNANT est : PLAYER 1`);
+			alert("Félicitations : le JOUEUR 1 a gagné");
 		} else {
 			globalPlayer1.innerText = global1;
 			displayPlayer(2);
@@ -133,7 +180,7 @@ function savePointsToGlobal() {
 	} else {
 		global2 = global2 + round2;
 		if (global2 >= 100) {
-			alert(`Le GAGNANT est : PLAYER 2`);
+			alert("Félicitations : le JOUEUR 2 a gagné");
 		} else {
 			globalPlayer2.innerText = global2;
 			displayPlayer(1);
@@ -144,39 +191,19 @@ function savePointsToGlobal() {
 	}
 }
 
-/////////////////////////////////////////////////////////////////
-
-//   Déroulement du Script
-
+/////////////////////////////////////////////////////////////o///
+// Déroulement du Script
 /////////////////////////////////////////////////////////////////
 
 // Message de Bienvenue quand le DOM est chargé
-// document.addEventListener("DOMContentLoaded", () => {
-// 	console.log("Hello, voici mon jeu en JS !");
-// });
+document.addEventListener("DOMContentLoaded", () => {
+	alert(
+		"BIENVENUE dans mon jeu de dé : démarrez en cliquant sur ROLLDICE. Le premier qui atteint 100 points à gagné !"
+	);
+});
 
-// Déclaration des variables du SCORE TOTAL des 2 joueurs
-const globalPlayer1 = document.getElementById("globalPlayer1");
-const globalPlayer2 = document.getElementById("globalPlayer2");
-
-// Déclaration des variables du SCORE du ROUND des 2 joueurs
-const roundPlayer1 = document.getElementById("roundPlayer1");
-const roundPlayer2 = document.getElementById("roundPlayer2");
-
-// Déclarations des variables pour l'affichage de la face du Dé
-const dots11 = document.querySelector(".dots11");
-const dots13 = document.querySelector(".dots13");
-const dots21 = document.querySelector(".dots21");
-const dots22 = document.querySelector(".dots22");
-const dots23 = document.querySelector(".dots23");
-const dots31 = document.querySelector(".dots31");
-const dots33 = document.querySelector(".dots33");
-
-// Déclaration des variables modifiables des scores ROUND et GLOBAL
-let global1 = 0;
-let global2 = 0;
-let round1 = 0;
-let round2 = 0;
+// Initialisation des scores
+resetGame();
 
 // Affichage du joueur qui démarre la partie (aléatoirement)
 let firstPlayer = getBeginner();
